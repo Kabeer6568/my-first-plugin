78b1c2ae05cd3c7a341c771b8fc95bf859a1186f
Kabeer Ali Alvi <kabeeralvi6568@gmail.com>
2025-08-16 02:55:05 +0500

    Made my first block in WordPress

---
+++ my-first-plugin/build/firstplugin/render.php
@@ -0,0 +1,18 @@
+<div class="services">
+
+  <div class="features">
+    <h1>Service 1</h1>
+    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Accusantium ut ratione ad expedita nemo voluptatum excepturi doloribus animi blanditiis, obcaecati consequuntur corrupti tempora, architecto sed deserunt omnis nulla, quidem vel.</p>
+  </div>
+
+  <div class="features">
+    <h1>Service 2</h1>
+    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Accusantium ut ratione ad expedita nemo voluptatum excepturi doloribus animi blanditiis, obcaecati consequuntur corrupti tempora, architecto sed deserunt omnis nulla, quidem vel.</p>
+  </div>
+  
+  <div class="features">
+    <h1>Service 3</h1>
+    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Accusantium ut ratione ad expedita nemo voluptatum excepturi doloribus animi blanditiis, obcaecati consequuntur corrupti tempora, architecto sed deserunt omnis nulla, quidem vel.</p>
+  </div>
+  
+</div>
---
+++ my-first-plugin/build/firstplugin/view.asset.php
@@ -0,0 +1,1 @@
+<?php return array('dependencies' => array(), 'version' => '2c6eaa652a4ff3e8a5d9');
